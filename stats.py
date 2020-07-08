bf100cf0d24d96ecd1c1346c7f79366601bdccf4
Colin Joseph Brown <39393709+colinjosephbrown@users.noreply.github.com>
2020-07-07 23:33:42 -0400

    Update stats.py

--- stats.py
+++ stats.py
@@ -12,15 +12,45 @@ HEIGHT = 'height'
 ANN_TYPE = 'type'
 ANN_SCORE = 'score'
 
+MAX_SCORE = 10
 
-def resize_img(cur_img, max_size=(660, 1300)):
-    ratio_0 = max_size[0] / float(cur_img.shape[0])
-    ratio_1 = max_size[1] / float(cur_img.shape[1])
-    ratio = min(ratio_0, ratio_1)
-    new_h = int(cur_img.shape[0] * ratio)
-    new_w = int(cur_img.shape[1] * ratio)
-    cur_img = cv2.resize(cur_img, (new_w, new_h))
-    return cur_img
+
+class Bbox:
+    def __init__(self, x, y, w, h):
+        self.xy = np.asarray([x, y])
+        self.wh = np.asarray([w, h])
+
+    @property
+    def x(self):
+        return self.xy[0]
+
+    @property
+    def y(self):
+        return self.xy[1]
+
+    @property
+    def width(self):
+        return self.wh[0]
+
+    @property
+    def height(self):
+        return self.wh[1]
+
+    @property
+    def x2(self):
+        return self.x + self.width
+
+    @property
+    def y2(self):
+        return self.y + self.height
+
+    @property
+    def xy2(self):
+        return np.asarray([self.x2, self.y2])
+
+    @property
+    def center(self):
+        return self.xy + self.wh / 2
 
 
 def convert_str(thestr):
@@ -34,91 +64,135 @@ def convert_str(thestr):
 
 
 class ImageStats:
-    def __init__(self):
+    def __init__(self, img_dir, tile_size=384, num_per_score=5):
+        self.img_dir = img_dir
+        self.tile_size = tile_size
+        self.num_per_score = num_per_score
+
         self.tiles = []
         self.anns_by_score = []
         self.score_col_index = {}
+        self.selected_tile = None
 
-    def create_chart(self, bg_img, w, n, img_dir, show_type='p'):
+        self.ui_img = None
+        self.bg_img = None
+
+        # Create the window
+        self.cv2_window_name = 'chart'
+        cv2.namedWindow(self.cv2_window_name, cv2.WINDOW_NORMAL)
+        cv2.setMouseCallback(self.cv2_window_name, self.mouse_event)
+
+        # Load the json db and compute the stats
+        fn = 'unscaled_annots.json'
+        self.compute_indexes_and_stats(fn, recompute_bbox=True)
+
+    def create_chart(self, show_type='p'):
         print("Creating chart...")
-        bg_size = (w * 11, w * (n + 1))
-        bg_img = cv2.resize(bg_img, bg_size)
+        bg_size = (self.tile_size * (MAX_SCORE + 1), self.tile_size * (self.num_per_score + 1))
+        self.bg_img = cv2.resize(self.bg_img, bg_size)
 
         # Create empty grid of tile metadata
-        self.tiles = 11 * [None]
+        self.tiles = (MAX_SCORE + 1) * [None]
         for score_col in range(len(self.tiles)):
-            self.tiles[score_col] = n * [None]
+            self.tiles[score_col] = self.num_per_score * [None]
 
-        type = show_type
+        for score in self.anns_by_score[show_type]:
+            self._draw_column(score, show_type)
 
-        for score in self.anns_by_score[type]:
-            if score not in self.score_col_index:
-                self.score_col_index[score] = 0
-            num_anns = len(self.anns_by_score[type][score])
-            max_i = min(n, num_anns)
+    def num_annotations(self, ann_type, score):
+        return len(self.anns_by_score[ann_type][score])
 
-            # Fill score column
-            i = 0
-            loop_count = 0
-            while i < max_i:
-                cur_index = self.score_col_index[score]
-                try:
-                    (img_name, ann) = self.anns_by_score[type][score][cur_index]
-                except IndexError as e:
-                    print(e)
-                if self.score_col_index[score] + 1 == num_anns:
-                    loop_count += 1
-                if loop_count == 2:
-                    break
+    def _draw_column(self, score, type):
+        if score not in self.score_col_index:
+            self.score_col_index[score] = 0
 
-                self.score_col_index[score] = (self.score_col_index[score] + 1) % num_anns
+        num_anns = self.num_annotations(ann_type=type, score=score)
+        max_num_rows = min(self.num_per_score, num_anns)
 
-                # Load image
+        # Fill score column
+        row_index = 0
+        loop_count = 0
+        while row_index < max_num_rows:
+            cur_index = self.score_col_index[score]
+            try:
+                (img_name, ann) = self.anns_by_score[type][score][cur_index]
                 img_name = convert_str(img_name)
-                print('New safe img_name')
-                raw_img = cv2.imread(os.path.join(img_dir, img_name))
-                if raw_img is None:
-                    print("Image " + img_name + " cannot be loaded...")
-                    continue
-                raw_img = resize_img(raw_img)
-                imh, imw = raw_img.shape[:2]
+            except IndexError as e:
+                print('cur_index: ' + str(cur_index))
+                print('score:' + str(score))
+                print('type:' + str(type))
+                print(e)
+                #break
 
-                # Make bbox square
-                if ann[BBOX][WIDTH] > ann[BBOX][HEIGHT]:
-                    ann[BBOX]['y'] -= (ann[BBOX][WIDTH] - ann[BBOX][HEIGHT]) / 2
-                    ann[BBOX][HEIGHT] = ann[BBOX][WIDTH]
-                else:
-                    ann[BBOX]['x'] -= (ann[BBOX][HEIGHT] - ann[BBOX][WIDTH]) / 2
-                    ann[BBOX][WIDTH] = ann[BBOX][HEIGHT]
+            if self.score_col_index[score] + 1 == num_anns:
+                loop_count += 1
 
-                # Crop to ann bbox
-                x1 = np.clip(ann[BBOX]['x'], 0, imw).astype(int)
-                x2 = np.clip(ann[BBOX]['x'] + ann[BBOX][WIDTH], 0, imw).astype(int)
-                y1 = np.clip(ann[BBOX]['y'], 0, imh).astype(int)
-                y2 = np.clip(ann[BBOX]['y'] + ann[BBOX][HEIGHT], 0, imh).astype(int)
-                if x2 <= x1 or y2 <= y1:
-                    print("Annotation has negative dim in image" + img_name)
-                    continue
-                try:
-                    cropped_img = raw_img[y1:y2, x1:x2]
-                except TypeError as e:
-                    print(str(x1) + ' ' + str(x2) + ' ' + str(y1) + ' ' + str(y2))
-                    raise
+            if loop_count == 2:
+                break
 
-                # Resize to 100x100
-                cropped_img = cv2.resize(cropped_img, (w, w))
+            self.score_col_index[score] = (self.score_col_index[score] + 1) % num_anns
 
-                # Draw at x, y
-                bg_img[i * w:(i + 1) * w, score * w:(score + 1) * w] = cropped_img
+            if not self._draw_tile(ann, score, img_name, row_index):
+                continue
 
-                # Set tile
+            # Increment index
+            row_index += 1
 
-                self.tiles[score][i] = {'file_name': img_name, 'annotation': ann}
+    def _draw_tile(self, ann, score, img_name, row_index):
+        img_path = os.path.join(self.img_dir, img_name)
+        raw_img = cv2.imread(img_path)
+        if raw_img is None:
+            print("Image " + img_name + " cannot be loaded...")
+            return False
 
-                # Increment index
-                i += 1
+        imh, imw = raw_img.shape[:2]
 
-        cv2.imshow(self.cv2_window_name, bg_img)
+        # Make bbox square
+        if ann[BBOX][WIDTH] > ann[BBOX][HEIGHT]:
+            ann[BBOX]['y'] -= (ann[BBOX][WIDTH] - ann[BBOX][HEIGHT]) / 2
+            ann[BBOX][HEIGHT] = ann[BBOX][WIDTH]
+        else:
+            ann[BBOX]['x'] -= (ann[BBOX][HEIGHT] - ann[BBOX][WIDTH]) / 2
+            ann[BBOX][WIDTH] = ann[BBOX][HEIGHT]
+
+        # Crop to ann bbox
+        x1 = np.clip(ann[BBOX]['x'], 0, imw).astype(int)
+        x2 = np.clip(ann[BBOX]['x'] + ann[BBOX][WIDTH], 0, imw).astype(int)
+        y1 = np.clip(ann[BBOX]['y'], 0, imh).astype(int)
+        y2 = np.clip(ann[BBOX]['y'] + ann[BBOX][HEIGHT], 0, imh).astype(int)
+        if x2 <= x1 or y2 <= y1:
+            print("Annotation has negative dim in image" + img_name)
+            return False
+        try:
+            cropped_img = raw_img[y1:y2, x1:x2]
+        except TypeError as e:
+            print(str(x1) + ' ' + str(x2) + ' ' + str(y1) + ' ' + str(y2))
+            raise
+
+        # Resize tile
+        cropped_img = cv2.resize(cropped_img, (self.tile_size, self.tile_size))
+
+        # Draw at x, y
+        tile_bbox = self.get_tile_bbox(score, row_index)
+        self.bg_img[tile_bbox.y:tile_bbox.y2, tile_bbox.x:tile_bbox.x2] = cropped_img
+
+        # Set tile
+        self.tiles[score][row_index] = {'file_name': img_name, 'annotation': ann}
+        return True
+
+    def get_tile_bbox(self, score, row_index):
+        # TODO: add offset and row/col padding
+        bbox = Bbox(x=score * self.tile_size,
+                    y=row_index * self.tile_size,
+                    w=self.tile_size,
+                    h=self.tile_size)
+        return bbox
+
+    def get_tile_col_row(self, x, y):
+        # TODO: add offset and row/col padding
+        tile_col = int(x / self.tile_size)
+        tile_row = int(y / self.tile_size)
+        return tile_col, tile_row
 
     def mouse_event(self, event, x, y, flags, param):
         if event == cv2.EVENT_LBUTTONDOWN:
@@ -126,14 +200,17 @@ class ImageStats:
         elif event == cv2.EVENT_MOUSEMOVE:
             pass
         elif event == cv2.EVENT_LBUTTONUP:
-            tile_x = int(x / self.tile_size)
-            tile_y = int(y / self.tile_size)
-            print('Tile[' + str(tile_x) + ', ' + str(tile_y) + ']: ' + self.tiles[tile_x][tile_y]['file_name'])
+            score, row_index = self.get_tile_col_row(x, y)
+            self.selected_tile = (score, row_index)
+            tile = self.tiles[score][row_index]
+            if tile is not None:
+                print('Tile[' + str(score) + ', ' + str(row_index) + ']: ' + tile['file_name'])
         elif event == cv2.EVENT_RBUTTONUP:
+            self.selected_tile = None
             pass
 
-    def set_selected_type(self, type):
-        self.create_chart(bg_img, w, n, img_dir, show_type=type)
+    #def set_selected_type(self, type):
+    #    self.create_chart(bg_img, show_type=type)
 
     def compute_indexes_and_stats(self, json_fn, recompute_bbox=False):
         data = json.load(open(json_fn, 'r'))
@@ -156,7 +233,7 @@ class ImageStats:
                 self.anns_by_score[ann[ANN_TYPE]][ann[ANN_SCORE]].append((img_name, ann))
 
                 if recompute_bbox:
-                    print('Recomputing bbox for annot in img ' + img_name)
+                    #print('Recomputing bbox for annot in img ' + img_name)
                     ann[BBOX] = get_bbox(np.array(ann['points']))
 
         # Output counts
@@ -171,53 +248,48 @@ class ImageStats:
                 output += str(counts[type][i]) + ', '
             print(output)
 
-    def main(self, args):
-        # Create the window
-        self.cv2_window_name = 'chart'
-        cv2.namedWindow(self.cv2_window_name, cv2.WINDOW_NORMAL)
-        cv2.setMouseCallback(self.cv2_window_name, self.mouse_event)
+    def main(self):
+        self.bg_img = cv2.imread('black.jpg')
 
-        # Load the json db and compute the stats
-        fn = 'unscaled_annots.json'
-        self.compute_indexes_and_stats(fn, recompute_bbox=True)
-
-        # Create sample chart
-        img_dir = os.path.join('movies', 'movies')
-        self.tile_size = 100
-        n = 5
-        bg_img = cv2.imread('black.jpg')
-
-        while (True):
+        while True:
+            new_annot_type = None
             key = cv2.waitKey(1) & 0xFF
 
-            if key == ord("p"):
-                self.cur_annot_type = 'p'
-                self.create_chart(bg_img, self.tile_size, n, img_dir, show_type=self.cur_annot_type)
-            elif key == ord("f"):
-                self.cur_annot_type = 'f'
-                self.create_chart(bg_img, self.tile_size, n, img_dir, show_type=self.cur_annot_type)
-            elif key == ord("t"):
-                self.cur_annot_type = 't'
-                self.create_chart(bg_img, self.tile_size, n, img_dir, show_type=self.cur_annot_type)
-            elif key == ord("h"):
-                self.cur_annot_type = 'h'
-                self.create_chart(bg_img, self.tile_size, n, img_dir, show_type=self.cur_annot_type)
-            elif key == ord("a"):
-                self.cur_annot_type = 'a'
-                self.create_chart(bg_img, self.tile_size, n, img_dir, show_type=self.cur_annot_type)
-            elif key == ord("c"):
-                self.cur_annot_type = 'c'
-                self.create_chart(bg_img, self.tile_size, n, img_dir, show_type=self.cur_annot_type)
-            elif key == ord("l"):
-                self.cur_annot_type = 'l'
-                self.create_chart(bg_img, self.tile_size, n, img_dir, show_type=self.cur_annot_type)
-            elif key == 27:
+            if key == 27:
                 break
+
+            annot_type_keys = ['a', 'c', 'f', 'h', 'l', 'p', 't']
+
+            for k in annot_type_keys:
+                if key == ord(k):
+                    new_annot_type = k
+
+            if new_annot_type is not None:
+                self.create_chart(show_type=new_annot_type)
+
+            # UI
+            draw_img = self.bg_img.copy()
+
+            if self.selected_tile is not None:
+                score = self.selected_tile[0]
+                row_index = self.selected_tile[1]
+                tile = self.tiles[score][row_index]
+
+                if tile is not None:
+                    tile_bbox = self.get_tile_bbox(score, row_index)
+                    cv2.rectangle(draw_img, pt1=tuple(tile_bbox.xy), pt2=tuple(tile_bbox.xy2), color=(255, 255, 0), thickness=3)
+
+                    cv2.putText(draw_img,
+                                str(score),
+                                tuple(tile_bbox.center.astype(int)),
+                                cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 0, 0), 2)
+
+            cv2.imshow(self.cv2_window_name, draw_img)
 
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument("--type", "-t", type=str)
     args = parser.parse_args()
-    stats = ImageStats()
-    stats.main(args)
+    stats = ImageStats(tile_size=384, num_per_score=5, img_dir=os.path.join('..', 'movies', 'movies'))
+    stats.main()
